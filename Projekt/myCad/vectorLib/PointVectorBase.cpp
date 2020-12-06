8984aa10f6801c81b7cb7ef76d55351e8b73e8e5
s0565952 <ahmed.abouismail96@gmail.com>
2020-12-06 16:37:10 +0100

    add code to pointvectorBase

--- Projekt/myCad/vectorLib/PointVectorBase.cpp
+++ Projekt/myCad/vectorLib/PointVectorBase.cpp
@@ -1,1 +1,59 @@
 #include "PointVectorBase.h"
+#include <cmath>
+#include "Object.h"
+#include <typeinfo>
+
+
+double PointVectorBase::BaseLength() {
+	return sqrt(pow(X, 2) + pow(Y, 2) + pow(Z, 2));
+};
+
+double PointVectorBase::BaseDistanceTo(PointVectorBase pvb) {
+	return sqrt(pow(pvb.X - X, 2) + pow(pvb.Y - Y, 2) + pow(pvb.Z - Z, 2));
+};
+
+void PointVectorBase::AddToThis(vector<PointVectorBase>pvbs) {
+	
+    for (PointVectorBase pvb : pvbs) {
+        X += pvb.X;
+        Y += pvb.Y;
+        Z += pvb.Z;
+    }
+};
+
+void PointVectorBase::MultiplyThisBy(double factor) {
+    X = X * factor;
+    Y = Y * factor;
+    Z = Z * factor;
+};
+
+//bool PointVectorBase::operator ==(PointVectorBase& p1, PointVectorBase& p2) {
+//    if (p1 == NULL &&  p2 == NULL)
+//    {
+//        return true;
+//    }
+//    if (p1 != nullptr && p2 != nullptr)
+//    {
+//        //Wenn Typen nicht gleich, immer false...
+//        if (typeid(p1).name() != typeid(p2).name()) {
+//            return false;
+//        }
+//        //Vergleich mit Toleranz in Würfel
+//        if (abs(p1->X - p2->X) > Tolerance) {
+//            return false;
+//        }
+//        if (abs(p1->Y - p2->Y) > Tolerance) {
+//            return false;
+//        }
+//        if (abs(p1->Z - p2->Z) > Tolerance) {
+//            return false;
+//        }
+//        return true;
+//        //oder Vergleich mit Toleranz in Kugel
+//        //return p1.BaseDistanceTo(p2) <= Tolerance;
+//    }
+//    return false;
+//}
+
+//bool PointVectorBase::operator !=(PointVectorBase p1, PointVectorBase p2) {
+//};
--- Projekt/myCad/vectorLib/PointVectorBase.h
+++ Projekt/myCad/vectorLib/PointVectorBase.h
@@ -1,9 +1,49 @@
 #pragma once
-// Abstract Class 
-class PointVectorBase
+// Abstract Class
+#include <vector>
+#include <string>
+#include <ostream>
+#include "Object.h"
+
+using namespace std;
+
+class PointVectorBase : public Object
 {
 public:
-	 PointVectorBase();
+	double X;
+	double Y;
+	double Z;
+	static double Tolerance;
+
+	PointVectorBase():PointVectorBase(0,0,0) {
+		
+	}
+	PointVectorBase(double x, double y, double z)
+	{
+		this->X = x;
+		this->Y = y;
+		this->Z = z;
+	};
+	//overload Operators
+	static bool operator ==(PointVectorBase& p1, PointVectorBase&p2);
+	static bool operator !=(PointVectorBase p1, PointVectorBase p2);
+
+
+	operator string() const;
+	bool Equals(Object obj);
+	int GetHashCode();
+
 	virtual ~PointVectorBase() {};
+protected:
+
+    double BaseLength();
+
+    double BaseDistanceTo(PointVectorBase pvb);
+
+	void AddToThis(vector<PointVectorBase>pvbs);
+
+	void MultiplyThisBy(double factor);
+
+	
 };
 
